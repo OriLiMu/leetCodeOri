226431978b1c67a6b0c8849dc64fdcfdcdbf040a
lovelife_lizhe <513278236@qq.com>
2025-08-05 12:39:23 +0800

    update

--- cppVersion/Template.cpp
+++ cppVersion/Template.cpp
@@ -1,16 +1,17 @@
 /**
- * Definition for singly-linked list.
- * struct ListNode {
+ * Definition for a binary tree node.
+ * struct TreeNode {
  *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    ListNode* sortList(ListNode* head) {
+    bool isValidBST(TreeNode* root) {
         
     }
 };
