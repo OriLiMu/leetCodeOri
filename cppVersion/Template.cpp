357bb589fd9e63abbe27138bb2541fd92fb75d3d
lovelife_lizhe <imlegendlzz@gmail.com>
2025-10-04 17:23:05 +0800

    update

--- cppVersion/Template.cpp
+++ cppVersion/Template.cpp
@@ -1,6 +1,26 @@
-class Solution {
+class Trie {
 public:
-    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+    Trie() {
+        
+    }
+    
+    void insert(string word) {
+        
+    }
+    
+    bool search(string word) {
+        
+    }
+    
+    bool startsWith(string prefix) {
         
     }
 };
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie* obj = new Trie();
+ * obj->insert(word);
+ * bool param_2 = obj->search(word);
+ * bool param_3 = obj->startsWith(prefix);
+ */
