26c37e2f9a5f6463ff8312d8eaf73889cc2ec013
lovelife_lizhe <imlegendlzz@gmail.com>
2025-10-05 02:21:52 +0800

    update

---
+++ cppVersion/46_Permutations_v2.cpp
@@ -0,0 +1,21 @@
+#include <algorithm>
+#include <climits>
+#include <cmath>
+#include <cstddef>
+#include <iostream>
+#include <iterator>
+#include <queue>
+#include <strings.h>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+class Solution {
+public:
+  vector<vector<int>> permute(vector<int> &nums) {}
+};
+int main() {
+  Solution s;
+  cout << "hello" << endl;
+}
