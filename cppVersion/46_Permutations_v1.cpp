adf62173bc4be927ed80ff1da76251d989bacfa1
lovelife_lizhe <imlegendlzz@gmail.com>
2025-11-15 18:56:19 +0800

    update

--- cppVersion/46_Permutations_v1.cpp
+++ cppVersion/46_Permutations_v1.cpp
@@ -3,37 +3,19 @@
 #include <cmath>
 #include <cstddef>
 #include <iostream>
+#include <iterator>
+#include <queue>
+#include <strings.h>
+#include <unordered_map>
+#include <unordered_set>
 #include <vector>
 
 using namespace std;
 class Solution {
 public:
-  void getPermute(vector<int> &nums, int startIndex,
-                  vector<vector<int>> &result) {
-    if (startIndex == nums.size()) {
-      result.push_back(nums);
-      return;
-    }
-    for (int i = startIndex; i < nums.size(); i++) {
-      swap(nums[i], nums[startIndex]);
-      getPermute(nums, startIndex + 1, result);
-      swap(nums[i], nums[startIndex]);
-    }
-  }
-  vector<vector<int>> permute(vector<int> &nums) {
-    vector<vector<int>> result;
-    getPermute(nums, 0, result);
-    return result;
-  }
+  vector<vector<int>> permute(vector<int> &nums) {}
 };
 int main() {
   Solution s;
-  vector<int> nums = {1, 2, 3};
-  vector<vector<int>> r = s.permute(nums);
-  for (vector<int> v : r) {
-    for (int n : v) {
-      cout << n;
-    }
-    cout << endl;
-  }
+  cout << "hello" << endl;
 }
