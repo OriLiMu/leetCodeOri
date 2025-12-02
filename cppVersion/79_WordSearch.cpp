aee0cd1c00c5817c88ee0cfb41f5156e2fbe281f
lovelife_lizhe <imlegendlzz@gmail.com>
2025-12-02 16:00:31 +0800

    update

--- cppVersion/79_WordSearch.cpp
+++ cppVersion/79_WordSearch.cpp
@@ -13,25 +13,92 @@
 using namespace std;
 class Solution {
 public:
-  bool func(vector<vector<char>> &board, string &word, string &cur, int r,
-            int c, int direction) {
+  bool func(vector<vector<char>> &board, string &word, string &cur,
+            vector<int> &curPath, int r, int c, int rowCnt, int colCnt) {
     if (cur == word)
       return true;
-    if (direction != 2 && r < board.size())
-      func(board, word, cur)
+
+    // 下面这个逻辑我理解的是合并不了, 需要单独的处理
+    // go up 有效性检验
+    if (r - 1 >= 0) {
+      int upIdx = (r - 1) * colCnt + c;
+      char upChar = board[r - 1][c];
+      if (upChar == word[cur.size()] &&
+          find(curPath.begin(), curPath.end(), upIdx) == curPath.end()) {
+        cur.push_back(upChar);
+        curPath.push_back(upIdx);
+        if (func(board, word, cur, curPath, r - 1, c, rowCnt, colCnt))
+          return true;
+        cur.pop_back();
+        curPath.pop_back();
+      }
+    }
+
+    if (r + 1 < rowCnt) {
+      int downIdx = (r + 1) * colCnt + c;
+      char downChar = board[r + 1][c];
+      if (downChar == word[cur.size()] &&
+          find(curPath.begin(), curPath.end(), downIdx) == curPath.end()) {
+        cur.push_back(downChar);
+        curPath.push_back(downIdx);
+        if (func(board, word, cur, curPath, r + 1, c, rowCnt, colCnt))
+          return true;
+        cur.pop_back();
+        curPath.pop_back();
+      }
+    }
+
+    if (c - 1 >= 0) {
+      int leftIdx = r * colCnt + c - 1;
+      char leftChar = board[r][c - 1];
+      if (leftChar == word[cur.size()] &&
+          find(curPath.begin(), curPath.end(), leftIdx) == curPath.end()) {
+        cur.push_back(leftChar);
+        curPath.push_back(leftIdx);
+        if (func(board, word, cur, curPath, r, c - 1, rowCnt, colCnt))
+          return true;
+        cur.pop_back();
+        curPath.pop_back();
+      }
+    }
+
+    if (c + 1 < colCnt) {
+      int rightIdx = r * colCnt + c + 1;
+      char rightChar = board[r][c + 1];
+      if (rightChar == word[cur.size()] &&
+          find(curPath.begin(), curPath.end(), rightIdx) == curPath.end()) {
+        cur.push_back(rightChar);
+        curPath.push_back(rightIdx);
+        if (func(board, word, cur, curPath, r, c + 1, rowCnt, colCnt))
+          return true;
+        cur.pop_back();
+        curPath.pop_back();
+      }
+    }
+
+    return false;
   }
+
   bool exist(vector<vector<char>> &board, string word) {
     int r = board.size();
     int c = board[0].size();
+    string cur;
+    vector<int> curPath;
     vector<vector<int>> accessedMap(r, vector<int>(c));
     for (int i = 0; i < r; ++i) {
       for (int j = 0; j < c; ++j) {
-        func(, vector<vector<char>> & board, string & word);
+        if (func(board, word, cur, curPath, i, j, r, c))
+          return true;
       }
     }
+
+    return false;
   }
 };
+
 int main() {
   Solution s;
-  cout << "hello" << endl;
+  vector<vector<char>> b = {
+      {'A', 'B', 'C', 'E'}, {'S', 'F', 'C', 'S'}, {'A', 'D', 'E', 'E'}};
+  cout << s.exist(b, "SEE") << endl;
 }
--- cppVersion/main.cpp
+++ cppVersion/main.cpp
@@ -13,49 +13,92 @@
 using namespace std;
 class Solution {
 public:
-  void func(int leftCnt, int rightCnt, string &cur, vector<char> &stack,
-            vector<string> &result) {
-    if (leftCnt == 0 && rightCnt == 0) {
-      result.push_back(cur);
+  bool func(vector<vector<char>> &board, string &word, string &cur,
+            vector<int> &curPath, int r, int c, int rowCnt, int colCnt) {
+    if (cur == word)
+      return true;
+
+    // 下面这个逻辑我理解的是合并不了, 需要单独的处理
+    // go up 有效性检验
+    if (r - 1 >= 0) {
+      int upIdx = (r - 1) * colCnt + c;
+      char upChar = board[r - 1][c];
+      if (upChar == word[cur.size()] &&
+          find(curPath.begin(), curPath.end(), upIdx) == curPath.end()) {
+        cur.push_back(upChar);
+        curPath.push_back(upIdx);
+        if (func(board, word, cur, curPath, r - 1, c, rowCnt, colCnt))
+          return true;
+        cur.pop_back();
+        curPath.pop_back();
+      }
     }
 
-    // 回复状态
-    if (leftCnt) {
-      cur.push_back('(');
-      stack.push_back('(');
-      func(leftCnt - 1, rightCnt, cur, stack, result);
-      stack.pop_back();
-      cur.pop_back();
+    if (r + 1 < rowCnt) {
+      int downIdx = (r + 1) * colCnt + c;
+      char downChar = board[r + 1][c];
+      if (downChar == word[cur.size()] &&
+          find(curPath.begin(), curPath.end(), downIdx) == curPath.end()) {
+        cur.push_back(downChar);
+        curPath.push_back(downIdx);
+        if (func(board, word, cur, curPath, r + 1, c, rowCnt, colCnt))
+          return true;
+        cur.pop_back();
+        curPath.pop_back();
+      }
     }
 
-    if (!stack.empty()) {
-      cur.push_back(')');
-      stack.pop_back();
-      func(leftCnt, rightCnt - 1, cur, stack, result);
-      cur.pop_back();
-      stack.push_back('(');
+    if (c - 1 >= 0) {
+      int leftIdx = r * colCnt + c - 1;
+      char leftChar = board[r][c - 1];
+      if (leftChar == word[cur.size()] &&
+          find(curPath.begin(), curPath.end(), leftIdx) == curPath.end()) {
+        cur.push_back(leftChar);
+        curPath.push_back(leftIdx);
+        if (func(board, word, cur, curPath, r, c - 1, rowCnt, colCnt))
+          return true;
+        cur.pop_back();
+        curPath.pop_back();
+      }
     }
 
-    return;
+    if (c + 1 < colCnt) {
+      int rightIdx = r * colCnt + c + 1;
+      char rightChar = board[r][c + 1];
+      if (rightChar == word[cur.size()] &&
+          find(curPath.begin(), curPath.end(), rightIdx) == curPath.end()) {
+        cur.push_back(rightChar);
+        curPath.push_back(rightIdx);
+        if (func(board, word, cur, curPath, r, c + 1, rowCnt, colCnt))
+          return true;
+        cur.pop_back();
+        curPath.pop_back();
+      }
+    }
+
+    return false;
   }
 
-  vector<string> generateParenthesis(int n) {
+  bool exist(vector<vector<char>> &board, string word) {
+    int r = board.size();
+    int c = board[0].size();
     string cur;
-    vector<char> stack;
-    vector<string> result;
+    vector<int> curPath;
+    vector<vector<int>> accessedMap(r, vector<int>(c));
+    for (int i = 0; i < r; ++i) {
+      for (int j = 0; j < c; ++j) {
+        if (func(board, word, cur, curPath, i, j, r, c))
+          return true;
+      }
+    }
 
-    func(n, n, cur, stack, result);
-
-    return result;
+    return false;
   }
 };
 
 int main() {
   Solution s;
-  int n = 3;
-  vector<string> r = s.generateParenthesis(n);
-  for (const auto &elem : r) {
-    cout << elem << " ";
-  }
-  cout << endl;
+  vector<vector<char>> b = {
+      {'A', 'B', 'C', 'E'}, {'S', 'F', 'C', 'S'}, {'A', 'D', 'E', 'E'}};
+  cout << s.exist(b, "SEE") << endl;
 }
