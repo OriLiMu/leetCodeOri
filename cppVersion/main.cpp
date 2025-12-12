c99ccaa5f01207138bab8f0931157ef0b7e31037
lovelife_lizhe <imlegendlzz@gmail.com>
2025-12-12 15:39:38 +0800

    update

--- cppVersion/main.cpp
+++ cppVersion/main.cpp
@@ -13,8 +13,39 @@
 using namespace std;
 class Solution {
 public:
+  vector<int> l1, l2;
+  void dfs(vector<string> &cur, string &s, int leftIdx,
+           vector<vector<string>> &r) {
+    // for (auto &s : cur) {
+    //   cout << s << ", ";
+    // }
+    // cout << endl;
+
+    if (leftIdx == s.size()) {
+      r.push_back(cur);
+      return;
+    }
+
+    for (int i = 0; i < l1.size(); i++) {
+      if (l1[i] <= leftIdx && i >= leftIdx) {
+        string t = s.substr(leftIdx, (i - leftIdx) * 2 + 1);
+        cur.push_back(t);
+        dfs(cur, s, i + i - leftIdx + 1, r);
+        cur.pop_back();
+      }
+    }
+
+    for (int i = 0; i + 1 < l2.size(); i++) {
+      if (l2[i] <= leftIdx && i >= leftIdx) {
+        string t = s.substr(leftIdx, (i - leftIdx + 1) * 2);
+        cur.push_back(t);
+        dfs(cur, s, i + 1 + i - leftIdx + 1, r);
+        cur.pop_back();
+      }
+    }
+  }
+
   vector<vector<string>> partition(string s) {
-    vector<int> l1, l2;
     int left, right;
     for (int i = 0; i < s.size(); ++i) {
       left = right = i;
@@ -30,7 +61,7 @@ public:
       left = right = i;
       right++;
       if (s[left] != s[right]) {
-        l2.push_back(-1);
+        l2.push_back(s.size());
         continue;
       }
 
@@ -42,16 +73,15 @@ public:
       l2.push_back(left);
     }
 
-    for (auto &cc : l2) {
-      cout << cc << ", ";
-    }
-    cout << endl;
-
     vector<vector<string>> r;
+    vector<string> cur;
+    dfs(cur, s, 0, r);
     return r;
   }
 };
 int main() {
   Solution s;
-  s.partition("AABBAA");
+  vector<vector<string>> r;
+  string t = "bb";
+  r = s.partition(t);
 }
