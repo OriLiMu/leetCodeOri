80f492e74b91ab9b7f09a767168e0b9dc2f2834e
lovelife_lizhe <imlegendlzz@gmail.com>
2025-12-14 16:42:55 +0800

    update

--- cppVersion/153_FindMinimuminRotatedSortedArray.cpp
+++ cppVersion/153_FindMinimuminRotatedSortedArray.cpp
@@ -13,9 +13,23 @@
 using namespace std;
 class Solution {
 public:
-  int findMin(vector<int> &nums) {}
+  int findMin(vector<int> &nums) {
+    int l = 0, r = nums.size() - 1, mid;
+    while (l < r) {
+      mid = (l + r) / 2;
+      if (nums[mid] >= nums[r])
+        l = mid + 1;
+      else
+        r = mid;
+    }
+
+    return nums[l];
+  }
 };
+
 int main() {
   Solution s;
-  cout << "hello" << endl;
+  vector<int> v = {4, 5, 0, 1, 2, 3};
+
+  cout << s.findMin(v) << endl;
 }
