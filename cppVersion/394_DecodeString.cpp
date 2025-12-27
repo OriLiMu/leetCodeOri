5cc84abe59949cf417694066f32e355dd30691bc
lovelife_lizhe <imlegendlzz@gmail.com>
2025-12-27 22:24:36 +0800

    update

--- cppVersion/394_DecodeString.cpp
+++ cppVersion/394_DecodeString.cpp
@@ -7,6 +7,7 @@
 #include <iterator>
 #include <queue>
 #include <strings.h>
+#include <type_traits>
 #include <unordered_map>
 #include <unordered_set>
 #include <vector>
@@ -17,25 +18,73 @@ public:
   struct node {
     string prestr;
     int cnt;
+    node(string s, int n) : prestr(s), cnt(n) {};
   };
 
   string decodeString(string s) {
-    string alphaStr;
-    string cntStr;
+    // 这个r的概念也有点乱
+    string alphaStr, cntStr, r;
     vector<node> stk;
     int idx = 0;
     char ch;
+    bool isLBOn = false;
     while (idx < s.size()) {
       while (isalpha(s[idx]))
-        alphaStr.push_back(s[idx++]);
+        if (isLBOn)
+          alphaStr.push_back(s[idx++]);
+        else
+          r += s[idx++];
+
       while (isdigit(s[idx]))
         cntStr.push_back(s[idx++]);
       if (s[idx] == '[') {
+        isLBOn = true;
+        int cnt = stoi(cntStr);
+        stk.push_back(node(alphaStr, cnt));
+        cntStr.clear();
+        alphaStr.clear();
+        idx++;
+      }
+
+      if (s[idx] == ']') {
+        node tn = stk.back();
+        string st = tn.prestr;
+        if (alphaStr.empty()) {
+          st = "";
+          alphaStr = tn.prestr;
+        }
+        // 你这里有一个预设的前提, 就是alphaStr一定走过了一些字母,
+        // 然而可能出现两个]]紧挨着的情况
+        for (int i = 0; i < tn.cnt; ++i)
+          st += alphaStr;
+        stk.pop_back();
+        if (stk.empty()) {
+          r += st;
+          isLBOn = false;
+        } else
+          stk.back().prestr += st;
+        alphaStr.clear();
+        cntStr.clear();
+        idx++;
       }
     }
+
+    return r;
   }
 };
+
 int main() {
   Solution s;
-  cout << "hello" << endl;
+  string sv = "3[a]2[bc]";
+  sv = "3[a]";
+  sv = "3[a2[c]]";
+  sv = "4[e1[f]]";
+  sv = "2[2[ye1[f]]]";
+  // 两数字相连的时候出现问题
+  sv = "2[ye2[1[f]]]";
+  // 这个就暴露出来你代码的问题, 你把上级和下级的字符串放一起了
+  // 逻辑理解顺序上有问题的代码是不能接受的
+  // 写一个代码之前先用语言描述出来你的做题的逻辑过程
+  // sv = "2[pq4[2[j]]]";
+  cout << s.decodeString(sv);
 }
--- cppVersion/main.cpp
+++ cppVersion/main.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <cctype>
 #include <climits>
 #include <cmath>
 #include <cstddef>
@@ -6,96 +7,83 @@
 #include <iterator>
 #include <queue>
 #include <strings.h>
+#include <type_traits>
 #include <unordered_map>
 #include <unordered_set>
-#include <utility>
 #include <vector>
 
 using namespace std;
 class Solution {
 public:
-  // 这个好像是错误的思路. 我的想法是用二分法在a上去做中点
-  // 没有错误只是思路不是我原本的二分法思路
-  double findMedianSortedArrays(vector<int> &nums1, vector<int> &nums2) {
-    int l1 = nums1.size(), l2 = nums2.size();
-    int rl = (l1 + l2) / 2;
-    int s1 = 0, s2 = 0;
-    bool isOdd = (l1 + l2) % 2 != 0;
-    while (rl > 0) {
-      if ((nums1.size() - s1) < (nums2.size() - s2)) {
-        swap(nums1, nums2);
-        swap(s1, s2);
-      }
+  struct node {
+    string prestr;
+    int cnt;
+    node(string s, int n) : prestr(s), cnt(n) {};
+  };
 
-      // 这个地方能不能去掉
-      if (s2 == nums2.size()) {
-        // 这里的理解就是还需要切掉几个
-        // 如果需要切掉一个, 那么当前的这个需要切掉, 也就是往前走一个位置
-        // 就是 s1 += rl
-        s1 += rl;
-        if (isOdd)
-          return nums1[s1];
+  string decodeString(string s) {
+    // 这个r的概念也有点乱
+    string alphaStr, cntStr, r;
+    vector<node> stk;
+    int idx = 0;
+    char ch;
+    bool isLBOn = false;
+    while (idx < s.size()) {
+      while (isalpha(s[idx]))
+        if (isLBOn)
+          alphaStr.push_back(s[idx++]);
         else
-          return (nums1[s1] + nums1[s1 - 1]) / 2.0;
+          r += s[idx++];
+
+      while (isdigit(s[idx]))
+        cntStr.push_back(s[idx++]);
+      if (s[idx] == '[') {
+        isLBOn = true;
+        int cnt = stoi(cntStr);
+        stk.push_back(node(alphaStr, cnt));
+        cntStr.clear();
+        alphaStr.clear();
+        idx++;
       }
 
-      l2 = min((int)nums2.size() - s2, rl / 2);
-      l1 = rl - l2;
-
-      // 这里的这个-1的问题还是需要注意
-      // 还是下标的边界问题
-      if (nums1[s1 + max(0, l1 - 1)] > nums2[s2 + max(l2 - 1, 0)]) {
-        s2 += max(l2, 1);
-        rl -= max(l2, 1); // 这里是推进循环的关键步骤, 这个变量l2可能为0
-      } else {
-        s1 += max(l1, 1);
-        rl -= max(l1, 1);
+      if (s[idx] == ']') {
+        node tn = stk.back();
+        string st = tn.prestr;
+        if (alphaStr.empty()) {
+          st = "";
+          alphaStr = tn.prestr;
+        }
+        // 你这里有一个预设的前提, 就是alphaStr一定走过了一些字母,
+        // 然而可能出现两个]]紧挨着的情况
+        for (int i = 0; i < tn.cnt; ++i)
+          st += alphaStr;
+        stk.pop_back();
+        if (stk.empty()) {
+          r += st;
+          isLBOn = false;
+        } else
+          stk.back().prestr += st;
+        alphaStr.clear();
+        cntStr.clear();
+        idx++;
       }
     }
 
-    // 这里的处理主要的内容还是对s1, s2有效性的判断.
-    // 能不能在上面处理这个有效性的问题
-    if (isOdd) {
-      if (s1 == nums1.size())
-        return nums2[s2];
-      if (s2 == nums2.size())
-        return nums1[s1]; // 这个地方手误了
-      return nums1[s1] < nums2[s2] ? nums1[s1] : nums2[s2];
-    } else {
-      // 这个式子是错的, s1, s2都是过位的都是后半段的index
-      // return (nums1[s1] + nums2[s2]) / 2.0;
-      // 这里需要验证数据有效性
-      // 非常麻烦
-      if (s1 == nums1.size()) {
-        if (s2 == 0)
-          return (nums1[s1 - 1] + nums2[s2]) / 2.0;
-        return (max(nums1[s1 - 1], nums2[s2 - 1]) + nums2[s2]) / 2.0;
-      } else if (s2 == nums2.size()) {
-        if (s1 == 0)
-          return (nums2[s2 - 1] + nums1[s1]) / 2.0;
-        return (max(nums1[s1 - 1], nums2[s2 - 1]) + nums1[s1]) / 2.0;
-      }
-
-      if (s1 == 0 && s2 == 0)
-        return (nums1[s1] + nums2[s2]) / 2.0;
-      else if (s1 == 0 && s2 != 0)
-        return (min(nums1[s1], nums2[s2]) + nums2[s2 - 1]) / 2.0;
-      else if (s1 != 0 && s2 == 0)
-        return (min(nums1[s1], nums2[s2]) + nums1[s1 - 1]) / 2.0;
-      return (min(nums1[s1], nums2[s2]) + max(nums1[s1 - 1], nums2[s2 - 1])) /
-             2.0;
-    }
+    return r;
   }
 };
 
 int main() {
   Solution s;
-  vector<int> v1 = {1, 2, 3, 4, 5};
-  vector<int> v2 = {6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17};
-  v1 = {};
-  v2 = {1, 2, 4, 5};
-  v1 = {1, 3};
-  v2 = {2};
-  cout << "Answer is: " << s.findMedianSortedArrays(v1, v2) << endl;
-  return 0;
+  string sv = "3[a]2[bc]";
+  sv = "3[a]";
+  sv = "3[a2[c]]";
+  sv = "4[e1[f]]";
+  sv = "2[2[ye1[f]]]";
+  sv = "2[ye2[1[f]]]";
+  // 这个就暴露出来你代码的问题, 你把上级和下级的字符串放一起了
+  // 逻辑理解顺序上有问题的代码是不能接受的
+  // 写一个代码之前先用语言描述出来你的做题的逻辑过程
+  // sv = "2[pq4[2[j]]]";
+  cout << s.decodeString(sv);
 }
