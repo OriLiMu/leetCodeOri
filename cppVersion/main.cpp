f86303bf41ff338fb8dc42c1eda2396dc8aff98e
lovelife_lizhe <imlegendlzz@gmail.com>
2026-01-16 17:23:57 +0800

    update

--- cppVersion/main.cpp
+++ cppVersion/main.cpp
@@ -16,12 +16,19 @@ class Solution {
 public:
   int jump(vector<int> &nums) {
     if (nums.size() == 1)
-      return 1;
+      return 0;
     int limit = nums[0], nextLimit = 0;
     int r = 0;
-    for (int i = 0; i < nums.size() && i <= limit; i++) {
-      nextLimit = max(i + nums[i], nextLimit);
-      if (i == limit) {
+    for (int i = 1; i < nums.size() && i <= limit; i++) {
+      if (limit >= nums.size() - 1) {
+        r++;
+        break;
+      }
+
+      // nextLimit 也许比开始的 limit 还要小.
+      nextLimit = max(limit, max(i + nums[i], nextLimit));
+      // 这个代码本身就有问题，要敏感一些处理这个代码。
+      if (i == limit) { // 这里有可能，已经超出范围了，可以直接返回。
         limit = nextLimit;
         r++;
       }
@@ -35,5 +42,7 @@ int main() {
   Solution s;
   vector<int> v = {2, 3, 1, 1, 4};
   v = {2, 3, 0, 1, 4};
+  // v = {2, 1};
+  v = {1, 2, 3};
   cout << s.jump(v);
 }
