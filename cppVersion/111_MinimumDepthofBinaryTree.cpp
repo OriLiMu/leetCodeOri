2fd4b7368f7fab0bef3653d24cb9a8fca6c8862c
lovelife_lizhe <imlegendlzz@gmail.com>
2026-01-21 00:05:45 +0800

    update

---
+++ cppVersion/111_MinimumDepthofBinaryTree.cpp
@@ -0,0 +1,30 @@
+#include <algorithm>
+#include <climits>
+#include <cmath>
+#include <cstddef>
+#include <iostream>
+#include <iterator>
+#include <queue>
+#include <strings.h>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
+};
+class Solution {
+public:
+  int minDepth(TreeNode *root) {}
+};
+int main() {
+  Solution s;
+  cout << "hello" << endl;
+}
