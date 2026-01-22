653105df8be011f36c37d374557cad2eb3af51cb
lovelife_lizhe <imlegendlzz@gmail.com>
2026-01-22 12:02:00 +0800

    update

--- cppVersion/45_JumpGameII_v3.cpp
+++ cppVersion/45_JumpGameII_v3.cpp
@@ -16,23 +16,28 @@ public:
   int jump(vector<int> &nums) {
     if (nums.size() == 1)
       return 0;
-    vector<int> v = {(int)nums.size() - 1};
-    for (int i = nums.size() - 2; i >= 0; --i) {
-      for (int j = 0; j < v.size(); ++j) {
-        if (nums[i] + i >= v[j]) {
-          while (v.size() > j + 1)
-            v.pop_back();
-          v.push_back(nums[i]);
-          break;
-        }
+
+    int limit = nums[0];
+    int r = 1, nextLimit = nums[0];
+    for (int i = 1; i <= limit; ++i) {
+      if (i == nums.size() - 1)
+        return r;
+      // 注意赋值顺序.
+      nextLimit = max(nextLimit, i + nums[i]);
+      if (i == limit) {
+        r++;
+        limit = nextLimit;
       }
     }
 
-    return v.size();
+    return r;
   }
 };
+
 int main() {
   Solution s;
   vector<int> v = {2, 3, 0, 1, 4};
+  v = {2, 3, 1, 1, 4};
+  v = {1, 1, 1, 1}; // 这个例子很有意思
   cout << s.jump(v);
 }
