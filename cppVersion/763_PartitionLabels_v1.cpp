3742f389f50bd8edbfeec3024c8943ed852cb53b
lovelife_lizhe <imlegendlzz@gmail.com>
2026-01-22 22:28:26 +0800

    update

--- cppVersion/763_PartitionLabels_v1.cpp
+++ cppVersion/763_PartitionLabels_v1.cpp
@@ -7,18 +7,21 @@ using namespace std;
 class Solution {
 public:
   vector<int> partitionLabels(string s) {
-    // unordered_map<char, int> d;
-    vector<int> d(26);
-    for (int i = 0; i < s.size(); ++i)
-      d[s[i] - 'a'] = i;
-    int l = 0, r = 0;
-    vector<int> result;
+    unordered_map<char, vector<int>> d;
     for (int i = 0; i < s.size(); ++i) {
-      r = max(r, d[s[i] - 'a']);
-      if (i == r) {
-        result.push_back(r - l + 1);
-        l = r + 1;
-      }
+      if (!d.contains(s[i]) || d[s[i]].size() == 1)
+        d[s[i]].push_back(i);
+      else
+        d[s[i]][1] = i;
+    }
+    int l = 0, r;
+    vector<int> result;
+    while (l < s.size()) {
+      r = d[s[l]].back();
+      for (int i = l; i <= r; i++)
+        r = max(r, d[s[i]].back());
+      result.push_back(r - l + 1);
+      l = r + 1;
     }
 
     return result;
