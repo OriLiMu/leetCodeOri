88fcfce1417bf2f60ed384649111e8d65335e00d
lovelife_lizhe <imlegendlzz@gmail.com>
2026-01-22 18:41:54 +0800

    update

---
+++ cppVersion/763_PartitionLabels.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <strings.h>
+#include <vector>
+
+using namespace std;
+class Solution {
+public:
+  vector<int> partitionLabels(string s) {}
+};
+int main() {
+  Solution s;
+  cout << "hello" << endl;
+}
