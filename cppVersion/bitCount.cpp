0a588f2f0507ed4897c127e4c60de2794b1fb56c
lovelife_lizhe <513278236@qq.com>
2026-02-11 15:47:45 +0800

    fix: 使用系统默认编译器g++, 修复for循环语法错误

--- cppVersion/bitCount.cpp
+++ cppVersion/bitCount.cpp
@@ -12,8 +12,8 @@ int main() {
   vector<int> v = {1, 2, 3};
   vector<int> c(32);
   for(auto& n: v){
-    for(int i = 0; i < 32; i++){
-      if(n & 1)
+    for(int i = 0; i < 32, i++){
+      if(n && 1)
         c[i]++;
       n >>= 1;
     }
