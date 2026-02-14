4c9a01345c520093e926f30f160f11d9b5696b49
lovelife_lizhe <513278236@qq.com>
2026-02-14 17:02:59 +0800

    update

--- cppVersion/bitCount.cpp
+++ cppVersion/bitCount.cpp
@@ -1,9 +1,9 @@
-#include <iostream>
-#include <vector>
-#include <chrono>
-#include <random>
-#include <iomanip>
 #include <algorithm>
+#include <chrono>
+#include <iomanip>
+#include <iostream>
+#include <random>
+#include <vector>
 
 using namespace std;
 using namespace chrono;
@@ -11,15 +11,15 @@ using namespace chrono;
 int main() {
   vector<int> v = {1, 2, 3};
   vector<int> c(32);
-  for(auto& n: v){
-    for(int i = 0; i < 32, i++){
-      if(n && 1)
+  for (auto n : v) {
+    for (int i = 0; i < 32; i++) {
+      if (n & 1)
         c[i]++;
       n >>= 1;
     }
   }
-  
-  for(int i = 0; i < 32; i++)
+
+  for (int i = 0; i < 32; i++)
     cout << i << " : " << c[i] << endl;
   return 0;
 }
