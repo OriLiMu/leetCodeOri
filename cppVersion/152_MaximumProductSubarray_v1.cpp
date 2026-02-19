2453787f2b4466d9a98412b3bb3f7cf432cd2761
lovelife_lizhe <513278236@qq.com>
2026-02-19 15:55:12 +0800

    update

--- cppVersion/152_MaximumProductSubarray_v1.cpp
+++ cppVersion/152_MaximumProductSubarray_v1.cpp
@@ -1,4 +1,3 @@
-#include <climits>
 #include <iostream>
 #include <strings.h>
 #include <vector>
@@ -8,42 +7,21 @@ class Solution {
 public:
   struct st {
     int cur_max;
-    int negv; // 如果没有怎么办
+    int last_negV;
   };
 
   int maxProduct(vector<int> &nums) {
-    vector<st> v(nums.size());
     int r = nums[0];
-    v[0].cur_max = nums[0];
-    v[0].negv = nums[0] < 0 ? nums[0] : 1;
+    vector<st> v(nums.size());
+    v[0] = {nums[0], 1};
     for (int i = 1; i < nums.size(); i++) {
-      if (nums[i] > 0) {
-        v[i].cur_max = max(v[i - 1].cur_max * nums[i], nums[i]);
-        if (v[i - 1].negv < 0)
-          v[i].negv = v[i - 1].negv * nums[i];
-      } else if (nums[i] == 0) {
-        v[i].cur_max = v[i - 1].negv * nums[i];
-        v[i].negv = 1;
-      } else {
-        if (v[i - 1].negv < 0) {
-          v[i].cur_max = v[i - 1].negv * nums[i];
-          // 这里假想这里一定是正的实际上可能是负的
-          if (v[i - 1].cur_max > 0)
-            v[i].negv = nums[i] * v[i - 1].cur_max;
-          else if (v[i - 1].cur_max < 0)
-            v[i].negv = nums[i];
-          else
-            v[i].negv = nums[i]; // 没考虑0的情况, 0 等于1
-        } else {
-          v[i].cur_max = max(v[i - 1].cur_max * nums[i], nums[i]);
-          if (v[i - 1].cur_max == 0)
-            v[i].negv = nums[i];
-          else // 这里cur_max只能是正的
-            v[i].negv = nums[i] * v[i - 1].cur_max;
+      if (nums[i] < 0) {
+        if (v[i - 1].last_negV >= 0) {
+          v[i].last_negV = nums[i] * v[i - 1].cur_max;
+          v[i].cur_max = max(nums[i], nums[i] * v[i - 1].cur_max);
         }
+        v[i] = max(v[i - 1] * nums[i], nums[i]);
       }
-
-      r = max(r, v[i].cur_max);
     }
 
     return r;
@@ -52,10 +30,9 @@ public:
 
 int main() {
   Solution s;
-  vector<int> v = {-3, 1, 2, 3, -2};
-  v = {1, 2, 3, 0, 0, 0, -2, -3, -10};
-  v = {-2};
-  // v = {-2, -3, -10};
-  // v = {0, -2, -3, -10};
-  cout << s.maxProduct(v);
+  vector<int> v = {2, 3, -2, 4};
+  v = {-2, 0, -1};
+  // 还是测试例不完善
+  v = {2, -5, -2, -4, 3};
+  cout << s.maxProduct(v) << endl;
 }
