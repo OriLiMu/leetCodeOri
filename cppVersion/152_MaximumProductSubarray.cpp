92c460e2db7663892729dc80b049cc77616e521d
lovelife_lizhe <513278236@qq.com>
2026-02-19 15:55:12 +0800

    update

--- cppVersion/152_MaximumProductSubarray.cpp
+++ cppVersion/152_MaximumProductSubarray.cpp
@@ -1,38 +1,65 @@
 #include <iostream>
+#include <ranges>
 #include <strings.h>
 #include <vector>
 
 using namespace std;
 class Solution {
 public:
-  struct st {
-    int cur_max;
-    int last_negV;
-  };
-
   int maxProduct(vector<int> &nums) {
-    int r = nums[0];
-    vector<st> v(nums.size());
-    v[0] = {nums[0], 1};
-    for (int i = 1; i < nums.size(); i++) {
-      if (nums[i] < 0) {
-        if (v[i - 1].last_negV >= 0) {
-          v[i].last_negV = nums[i] * v[i - 1].cur_max;
-          v[i].cur_max = max(nums[i], nums[i] * v[i - 1].cur_max);
-        }
-        v[i] = max(v[i - 1] * nums[i], nums[i]);
+    int neg_cnt = 0, headn = 1, tailn = 1, headp = 1, tailp = 1;
+    int r = nums[0], cur_p = 1, cur_len = 0;
+
+    if (nums.size() == 1)
+      return nums[0];
+
+    for (auto &n : nums) {
+      if (n == 0) {
+        if (cur_len == 0)
+          continue;
+        if (headn < 0)
+          r = max(r, max(cur_p / (headn * headp), cur_p / (tailn * tailp)));
+        r = max(r, 0);
+        neg_cnt = 0, headn = 1, tailn = 1, headp = 1, tailp = 1;
+        cur_p = 1;
+      } else if (n < 0) {
+        if (headn > 0)
+          headn = n;
+        tailn = n;
+        neg_cnt++;
+        cur_p *= n;
+        cur_len++;
+        r = max(r, cur_p);
+        tailp = 1;
+      } else {
+        if (headn > 0)
+          headp += n;
+        else
+          tailp *= n;
+        cur_p *= n;
+        cur_len++;
+        r = max(r, cur_p);
       }
     }
+
+    // cout << cur_p << ", " << headn << ", " << headp << ", " << tailn << ", "
+    //      << tailp << endl;
+    // 还需要判断 headp 和 tailp 的长度
+    r = max(r, max(cur_p / (headn * headp), cur_p / (tailn * tailp)));
 
     return r;
   }
 };
-
 int main() {
   Solution s;
-  vector<int> v = {2, 3, -2, 4};
+  vector<int> v = {1, 2, 3};
+  // v = {2};
+  // v = {-2};
+  // v = {1, 2, 3, 0};
+  v = {-1, -2, -4};
+  v = {-2, -3, 7};
+  v = {-2, 4, -3, 1, 2, -10, 7};
+  v = {2, 3, -2, 4};
   v = {-2, 0, -1};
-  // 还是测试例不完善
-  v = {2, -5, -2, -4, 3};
   cout << s.maxProduct(v) << endl;
 }
