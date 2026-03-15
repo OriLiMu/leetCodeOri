bd6962e1dc6b9b225e3b99807e9fe82139502fbd
lovelife_lizhe <imlegendlzz@gmail.com>
2026-03-15 12:01:31 +0800

    update

--- cppVersion/main.cpp
+++ cppVersion/main.cpp
@@ -1,38 +1,38 @@
-#include <algorithm>
-#include <climits>
-#include <cmath>
-#include <cstddef>
 #include <iostream>
-#include <iterator>
-#include <queue>
-#include <stack>
 #include <strings.h>
-#include <unordered_map>
-#include <unordered_set>
 #include <vector>
 
 using namespace std;
 class Solution {
 public:
-  int findDuplicate(vector<int> &nums) {
-    int slow = 0, fast = 0;
-    do {
-      slow = nums[slow];
-      fast = nums[nums[fast]];
-    } while (slow != fast);
-    fast = 0;
+  int longestCommonSubsequence(string text1, string text2) {
+    vector<vector<int>> dp(text1.size() + 1, vector<int>(text2.size() + 1));
 
-    while (slow != fast) {
-      slow = nums[slow];
-      fast = nums[fast];
+    for (int m = 0; m < text1.size(); ++m) {
+      for (int k = 0; k < text2.size(); ++k) {
+        // if (text1[m] == text2[k] && k > ce2) { // 错在把条件写在一起了
+        if (text1[m] == text2[k]) {
+          // 矛盾在于, 如果你跳了K, 那么之前的数据没有不全,
+          // 如果你补全那么就是 3-nested
+          // 错误的根源还是你想取巧
+          int t = max(dp[m + 1][k], dp[m][k + 1]) + 1;
+          for (int n = k; n < text2.size(); n++)
+            dp[m + 1][n + 1] = t;
+          break;
+        } else
+          dp[m + 1][k + 1] = max(dp[m + 1][k], dp[m][k + 1]);
+      }
     }
 
-    return fast;
+    return dp[text1.size()][text2.size()];
   }
 };
-
 int main() {
   Solution s;
-  vector<int> v = {1, 3, 4, 2, 2};
-  cout << s.findDuplicate(v) << endl;
+  string s1, s2;
+  s1 = "abcba";
+  s2 = "abcbcba";
+  // s1 = "abcd";
+  // s2 = "aced";
+  cout << s.longestCommonSubsequence(s1, s2) << endl;
 }
