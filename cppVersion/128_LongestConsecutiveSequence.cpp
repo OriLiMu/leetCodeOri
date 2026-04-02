13228012e11fff2d8438272b6c9516823fa2beed
lovelife_lizhe <imlegendlzz@gmail.com>
2026-04-02 11:43:25 +0800

    update

--- cppVersion/128_LongestConsecutiveSequence.cpp
+++ cppVersion/128_LongestConsecutiveSequence.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <iostream>
 #include <strings.h>
 #include <unordered_map>
@@ -9,14 +10,58 @@ public:
   int longestConsecutive(vector<int> &nums) {
     unordered_map<int, int> u;
     for (auto &n : nums) {
+      // 这个代码的一个关键逻辑问题就是
+      // 你想用下面这个代码去屏蔽重复的key.
+      // 但是你后面的操作没有保证每个key都有添加进去
+      // 这样想如果使用 erase是否是逻辑更加清楚的做法
+      if (u.contains(n))
+        continue;
       bool le = u.contains(n + 1);
       bool re = u.contains(n - 1);
-      if (u.contains(le) && u.contains(re)) {
-      }
+      if (le && re) {
+        int rre = u[n + 1];
+        int rle = u[n - 1];
+        u[rre] = rle;
+        u[rle] = rre;
+        // 注意这个步骤没有添加 n, 所以后面, 如果有重复的n进来还会有问题
+        // 总体还是逻辑的混乱
+        u[n] = n;
+
+        // cout << "k:" << rre << ", v:" << u[n + 1] << endl;
+        // cout << "k:" << rle << ", v:" << u[n - 1] << endl;
+      } else if (le) {
+        u[n] = u[n + 1];
+        u[u[n + 1]] = n;
+        // cout << "k1:" << n << ", v:" << u[n + 1] << endl;
+        // cout << "k1:" << u[n + 1] << ", v:" << n << endl;
+      } else if (re) {
+        u[n] = u[n - 1];
+        u[u[n - 1]] = n; // 这步已经把对应的值clear
+        // cout << "k2:" << n << ", v:" << u[n - 1] << endl;
+        // cout << "k2:" << u[n - 1] << ", v:" << n << endl;
+      } else
+        u[n] = n;
     }
+
+    int maxlen = 0;
+    for (auto &kv : u) {
+      // 这里少了一个绝对值的问题, 但是这里又需要考虑如果有负数的情况.
+      maxlen = max(kv.second - kv.first + 1, maxlen);
+      cout << kv.first << ", " << kv.second << endl;
+    }
+
+    cout << "final : " << maxlen << endl;
+    return maxlen;
   }
 };
 int main() {
   Solution s;
-  cout << "hello" << endl;
+  vector<int> v = {100, 4, 200, 1, 2};
+  v = {0, 3, 7, 2, 5, 8, 4, 6, 0, 1};
+  // 这个代码有个问题就是没考虑重复出现的点, 如果有重复出现的点可能删除之前的值
+  v = {1, 0, 1, 2};
+  // 差一个 u[n] = n, 那么这个意思是不是需要erase, 逻辑更干净
+  v = {-6, 6, -9, -7, 0,  3, 4,  -2, 2,  -1, 9, -9, 5, -3,
+       6,  1, 5,  -1, -2, 9, -9, -4, -6, -5, 6, -1, 3};
+  s.longestConsecutive(v);
 }
--- cppVersion/main.cpp
+++ cppVersion/main.cpp
@@ -7,30 +7,57 @@
 using namespace std;
 class Solution {
 public:
-  vector<vector<string>> groupAnagrams(vector<string> &strs) {
-    unordered_map<string, vector<string>> u;
-    vector<vector<string>> r;
-    for (auto &str : strs) {
-      string sorted = str;
-      sort(sorted.begin(), sorted.end());
-      u[sorted].push_back(str);
+  int longestConsecutive(vector<int> &nums) {
+    unordered_map<int, int> u;
+    for (auto &n : nums) {
+      if (u.contains(n))
+        continue;
+      bool le = u.contains(n + 1);
+      bool re = u.contains(n - 1);
+      if (le && re) {
+        int rre = u[n + 1];
+        int rle = u[n - 1];
+        u[rre] = rle;
+        u[rle] = rre;
+        // 注意这个步骤没有添加 n, 所以后面, 如果有重复的n进来还会有问题
+        // 总体还是逻辑的混乱
+        u[n] = n;
+
+        // cout << "k:" << rre << ", v:" << u[n + 1] << endl;
+        // cout << "k:" << rle << ", v:" << u[n - 1] << endl;
+      } else if (le) {
+        u[n] = u[n + 1];
+        u[u[n + 1]] = n;
+        // cout << "k1:" << n << ", v:" << u[n + 1] << endl;
+        // cout << "k1:" << u[n + 1] << ", v:" << n << endl;
+      } else if (re) {
+        u[n] = u[n - 1];
+        u[u[n - 1]] = n; // 这步已经把对应的值clear
+        // cout << "k2:" << n << ", v:" << u[n - 1] << endl;
+        // cout << "k2:" << u[n - 1] << ", v:" << n << endl;
+      } else
+        u[n] = n;
     }
 
-    for (auto &it : u) {
-      r.push_back(it.second);
+    int maxlen = 0;
+    for (auto &kv : u) {
+      // 这里少了一个绝对值的问题, 但是这里又需要考虑如果有负数的情况.
+      maxlen = max(kv.second - kv.first + 1, maxlen);
+      cout << kv.first << ", " << kv.second << endl;
     }
 
-    return r;
+    cout << "final : " << maxlen << endl;
+    return maxlen;
   }
 };
 int main() {
   Solution s;
-  vector<string> v = {"eat", "tea", "tan", "ate", "nat", "bat"};
-  vector<vector<string>> r = s.groupAnagrams(v);
-  for (const auto &row : r) {
-    for (const auto &elem : row) {
-      cout << elem << " ";
-    }
-    cout << endl;
-  }
+  vector<int> v = {100, 4, 200, 1, 2};
+  v = {0, 3, 7, 2, 5, 8, 4, 6, 0, 1};
+  // 这个代码有个问题就是没考虑重复出现的点, 如果有重复出现的点可能删除之前的值
+  v = {1, 0, 1, 2};
+  // 差一个 u[n] = n, 那么这个意思是不是需要erase, 逻辑更干净
+  v = {-6, 6, -9, -7, 0,  3, 4,  -2, 2,  -1, 9, -9, 5, -3,
+       6,  1, 5,  -1, -2, 9, -9, -4, -6, -5, 6, -1, 3};
+  s.longestConsecutive(v);
 }
