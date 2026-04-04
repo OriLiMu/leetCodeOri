b7784e11e138343373e88b8bf46b1eaa6dea6a95
lovelife_lizhe <513278236@qq.com>
2026-04-04 09:07:22 +0800

    update

---
+++ cppVersion/128_LongestConsecutiveSequence.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <strings.h>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+class Solution {
+public:
+  int longestConsecutive(vector<int> &nums) {
+    unordered_map<int, int> us;
+    for (auto &n : nums) {
+      bool le = us.contains();
+      if ()
+    }
+  }
+};
+
+int main() {
+  Solution s;
+  cout << "hello" << endl;
+}
+
--- cppVersion/1_TwoSum.cpp
+++ cppVersion/1_TwoSum.cpp
@@ -1,24 +1,22 @@
-#include <algorithm>
-#include <climits>
-#include <cmath>
-#include <cstddef>
 #include <iostream>
-#include <vector>
-#include <unordered_map>
-#include <unordered_set>
-#include <iterator>
 #include <strings.h>
-#include <stack>
-#include <queue>
+#include <unordered_map>
+#include <vector>
 
 using namespace std;
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
+  vector<int> twoSum(vector<int> &nums, int target) {
+    unordered_map<int, int> u;
+    for (int i = 0; i < nums.size(); ++i) {
+      if (u.find(target - nums[i]) != u.end())
+        return {i, nums[target - nums[i]]};
+      else
+        u.insert({nums[i], i});
     }
+  }
 };
 int main() {
   Solution s;
   cout << "hello" << endl;
 }
-            
