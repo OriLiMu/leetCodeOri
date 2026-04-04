7dedd548853f753870656532728001fdd522fe48
lovelife_lizhe <imlegendlzz@gmail.com>
2026-04-04 18:53:29 +0800

    update

--- cppVersion/438_FindAllAnagramsinaString.cpp
+++ cppVersion/438_FindAllAnagramsinaString.cpp
@@ -8,6 +8,10 @@ public:
   vector<int> findAnagrams(string s, string p) {}
 };
 int main() {
+  // 1. 输入: s = "cbaebabacd", p = "abc"
+  // 输出: {0,6}
+  // 2. 输入: s = "abab", p = "ab"
+  // 输出: {0,1,2}
   Solution s;
   cout << "hello" << endl;
 }
--- cppVersion/Template.cpp
+++ cppVersion/Template.cpp
@@ -1,6 +1,21 @@
-class Solution {
+class LRUCache {
 public:
-    vector<int> findAnagrams(string s, string p) {
+    LRUCache(int capacity) {
+        
+    }
+    
+    int get(int key) {
+        
+    }
+    
+    void put(int key, int value) {
         
     }
 };
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
