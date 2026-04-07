847e578d1eca37cf70e258f026e2f425c52ec986
lovelife_lizhe <imlegendlzz@gmail.com>
2026-04-07 09:58:09 +0800

    update

---
+++ cppVersion/238_ProductofArrayExceptSelf.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+class Solution {
+public:
+  vector<int> productExceptSelf(vector<int> &nums) {}
+};
+int main() {
+  // 1. 输入: nums = [1,2,3,4]
+  // 输出: {24,12,8,6}
+  // 2. 输入: nums = [-1,1,0,-3,3]
+  // 输出: {0,0,9,0,0}
+  Solution s;
+  cout << "hello" << endl;
+}
